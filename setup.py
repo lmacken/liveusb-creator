cd45aaa1f62b2318883902b1713a2ea55618e037
Luke Macken <lmacken@redhat.com>
2008-02-04 00:09:05 -0500

    Include syslinux and 7-Zip in our setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,12 @@ setup(
     console = [
         {
             "script" : "livecd-iso-to-usb.py",
-            "icon_resources" : [(0, "fedora.ico")]
+            "icon_resources" : [(0, "fedora.ico")],
         }
+    ],
+    data_files = [
+        "syslinux.exe",
+        ("7-Zip", ["7-Zip/7z.exe", "7-Zip/7z.dll", "7-Zip/7zCon.sfx",
+                   "7-Zip/License.txt", "7-Zip/copying.txt"]),
     ],
 )
