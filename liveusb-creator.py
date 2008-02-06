37817f778f44c8860e851e9ccb73cd99f38e5688
Luke Macken <lmacken@redhat.com>
2008-02-06 14:44:56 -0500

    Update our copyright and license

--- liveusb-creator.py
+++ liveusb-creator.py
@@ -1,22 +1,25 @@
+# -*- coding: utf-8 -*-
+#
 # This tool installs a Fedora Live ISO (F7+) on to a USB stick, from Windows.
 # For information regarding the installation of Fedora on USB drives, see
 # the wiki: http://fedoraproject.org/wiki/FedoraLiveCD/USBHowTo
 #
-# Copyright 2008  Red Hat, Inc.
-# Authors: Luke Macken <lmacken@redhat.com>
+# Copyright © 2008  Red Hat, Inc. All rights reserved.
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 2 of the License.
+# This copyrighted material is made available to anyone wishing to use, modify,
+# copy, or redistribute it subject to the terms and conditions of the GNU
+# General Public License v.2.  This program is distributed in the hope that it
+# will be useful, but WITHOUT ANY WARRANTY expressed or implied, including the
+# implied warranties of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.  You should have
+# received a copy of the GNU General Public License along with this program; if
+# not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA. Any Red Hat trademarks that are
+# incorporated in the source code or documentation are not subject to the GNU
+# General Public License and may only be used or replicated with the express
+# permission of Red Hat, Inc.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Library General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# Red Hat Author(s): Luke Macken <lmacken@redhat.com>
 
 import win32file
 import win32api
