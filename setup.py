a0a67cd754edb0a05d1832e8468fd632bbafab21
Luke Macken <lmacken@redhat.com>
2008-02-08 03:57:25 -0500

    Update our setup.py to pull in PyQt4 when we py2exe

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from distutils.core import setup
 import py2exe
 
 setup(
-    console = [
+    windows = [
         {
-            "script" : "liveusb-creator.py",
-            "icon_resources" : [(0, "data/fedora.ico")],
+            "script" : "livecd-iso-to-usb.py",
+            "icon_resources" : [(0, "fedora.ico")],
         }
     ],
+    options={"py2exe" : {"includes" : ["sip", "PyQt4._qt"]}}, 
     data_files = [
-        "data/syslinux.exe",
-        ("data/7-Zip", ["7-Zip/7z.exe", "7-Zip/7z.dll", "7-Zip/7zCon.sfx",
-                        "7-Zip/License.txt", "7-Zip/copying.txt"]),
+        "syslinux.exe",
+        ("7-Zip", ["7-Zip/7z.exe", "7-Zip/7z.dll", "7-Zip/7zCon.sfx",
+                   "7-Zip/License.txt", "7-Zip/copying.txt"]),
     ],
 )
