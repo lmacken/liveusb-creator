2cd5f1a64478737995edf14657bfabbd6d173d34
Luke Macken <lmacken@redhat.com>
2008-04-25 01:38:11 -0400

    Pull our releases out into a separate module

---
+++ liveusb/releases.py
@@ -0,0 +1,42 @@
+releases = (
+    {
+        'name': 'Fedora 9 Preview (i686)',
+        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/test/9-Preview/Live/i686/Fedora-9-Preview-i686-Live.iso',
+        'sha1': '8879964cb31470d1f5dc1dd814e357165564e0de',
+    },
+    {
+        'name': 'Fedora 9 Preview (x86_64)',
+        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/test/9-Preview/Live/x86_64/Fedora-9-Preview-x86_64-Live.iso',
+        'sha1': '6b10ce1f780bb93cbe9923589012f759b46906a6',
+    },
+    {
+        'name': 'Fedora 9 KDE Preview (i686)',
+        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/test/9-Preview/Live/i686/Fedora-9-Preview-i686-Live-KDE.iso',
+        'sha1': 'bf03bb213ae2e622f38811eba963c161c6d67b78',
+    },
+    {
+        'name': 'Fedora 9 KDE Preview (x86_64)',
+        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/test/9-Preview/Live/x86_64/Fedora-9-Preview-x86_64-Live-KDE.iso',
+        'sha1': '5ffa1418854f18d52c9a072cd32d2c5a0e315e6b',
+    },
+    {
+        'name': 'Fedora 8 (i686)',
+        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/8/Live/i686/Fedora-8-Live-i686.iso',
+        'sha1':  'afc54d62424f36e377fc0440227ccc2de21e2a58',
+    },
+    {
+        'name': 'Fedora 8 (x86_64)',
+        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/8/Live/x86_64/Fedora-8-Live-x86_64.iso',
+        'sha1':  '51733eba9eca6e7b4c92677b8f3da2b8f8ac5a9a',
+    },
+    {
+        'name': 'Fedora 8 KDE (i686)',
+        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/8/Live/i686/Fedora-8-Live-KDE-i686.iso',
+        'sha1': '74c8ae2529ddbb1b56ceb1a7091ea4475d3f45f9',
+    },
+    {
+        'name': 'Fedora 8 (x86_64)',
+        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/8/Live/x86_64/Fedora-8-Live-KDE-x86_64.iso',
+        'sha1':  '9b7e08afe5dc919be3ba8c146261d684b9327556',
+    },
+)
