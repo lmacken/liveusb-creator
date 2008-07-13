c07d292d529c94173b0f457cb0afc19c92e6c003
Luke Macken <lmacken@redhat.com>
2008-07-13 01:23:15 -0400

    Update our setup.py to handle distutils.  Patch from caio1982@gmail.com (#94)

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from distutils.core import setup
-import py2exe
+try:
+    import py2exe
+except ImportError:
+    pass
 
 setup(
+    name = 'liveusb-creator',
+    version = '0.3.0',
+    packages = ['liveusb', 'liveusb/urlgrabber'],
+    scripts = ['liveusb-creator'], 
+    license = 'GNU General Public License (GPL)',
+    url = 'https://fedorahosted.org/liveusb-creator',
+    description = 'This tool installs a LiveCD ISO on to a USB stick',
+    long_description = 'The liveusb-creator is a cross-platform tool for easily installing live operating systems on to USB flash drives',
+    platforms = ['any'], 
+    maintainer = 'Luke Macken',
+    maintainer_email = 'lmacken@redhat.com',
     windows = [
         {
             "script" : "liveusb-creator",
