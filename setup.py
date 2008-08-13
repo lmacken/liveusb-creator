6019c53be1f76e5ae64aff49eafc6d0347dc441d
Kushal Das <kdas@d80.(none)>
2008-08-13 21:34:18 +0530

    tried to write a PolicyKit policy file, which is not working btw :)

--- setup.py
+++ setup.py
@@ -53,6 +53,10 @@ else:
         platforms = ['Linux'],
         maintainer = 'Luke Macken',
         maintainer_email = 'lmacken@redhat.com',
-        data_files = [("/usr/share/applications",["data/liveusb-creator.desktop"]), ('/usr/share/pixmaps',["data/fedora.ico"])]
+        data_files = [("/usr/share/applications",["data/liveusb-creator.desktop"]), 
+                      ('/usr/share/pixmaps',["data/fedora.ico"]),
+                      ('/usr/share/PolicyKit/policy',["liveusb-creator.policy"])
+                      ]
+        
         )
 
