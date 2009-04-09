aec98e08d99283699908f4e8a000e88bcdc86357
Luke Macken <lmacken@redhat.com>
2009-04-09 14:04:26 -0400

    Fedora 8 is EOL'd

--- liveusb/releases.py
+++ liveusb/releases.py
@@ -62,26 +62,6 @@ releases = (
         'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/9/Live/x86_64/Fedora-9-x86_64-Live-KDE.iso',
         'sha1': 'ebb037e50b16e9e4741c49b98a44e223cf77e236',
     },
-    {
-        'name': 'Fedora 8 (i686)',
-        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/8/Live/i686/Fedora-8-Live-i686.iso',
-        'sha1':  'afc54d62424f36e377fc0440227ccc2de21e2a58',
-    },
-    {
-        'name': 'Fedora 8 (x86_64)',
-        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/8/Live/x86_64/Fedora-8-Live-x86_64.iso',
-        'sha1':  '51733eba9eca6e7b4c92677b8f3da2b8f8ac5a9a',
-    },
-    {
-        'name': 'Fedora 8 KDE (i686)',
-        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/8/Live/i686/Fedora-8-Live-KDE-i686.iso',
-        'sha1': '74c8ae2529ddbb1b56ceb1a7091ea4475d3f45f9',
-    },
-    {
-        'name': 'Fedora 8 KDE (x86_64)',
-        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/8/Live/x86_64/Fedora-8-Live-KDE-x86_64.iso',
-        'sha1':  '9b7e08afe5dc919be3ba8c146261d684b9327556',
-    },
 
     ##
     ## Custom spins
