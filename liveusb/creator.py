537cff6bf344bad6308e88fd466ace3bf5a5a0be
Luke Macken <lmacken@redhat.com>
2009-06-19 02:38:54 -0400

    Don't try and unmount th device every time the window is closed

--- liveusb/creator.py
+++ liveusb/creator.py
@@ -632,7 +632,7 @@ class LinuxLiveUSBCreator(LiveUSBCreator):
                 self.log.debug("Killed process %d" % pid)
             except OSError:
                 pass
-        self.unmount_device()
+        #self.unmount_device()
 
     def verify_iso_md5(self):
         """ Verify the ISO md5sum.
