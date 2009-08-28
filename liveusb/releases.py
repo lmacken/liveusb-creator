9889c42e279753a945f92478ba0a04146e0626b0
Luke Macken <lmacken@redhat.com>
2009-08-27 21:25:03 -0400

    Add F12 alpha and remove F9

--- liveusb/releases.py
+++ liveusb/releases.py
@@ -1,4 +1,5 @@
 releases = (
+
     {
         'name': 'Fedora 11 (i686)',
         'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/11/Live/i686/Fedora-11-i686-Live.iso',
@@ -21,6 +22,30 @@ releases = (
     },
 
     {
+        'name': 'Fedora 12 Alpha (i686)',
+        'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/test/12-Alpha/Live/i686/F12-Alpha-i686-Live.iso',
+        'sha256': 'c0b229070f04197c3f0a92fed3f7fda95535b2c2a6fc5d80db2c4e35e0a69482',
+    },
+
+    {
+        'name': 'Fedora 12 Alpha KDE (i686)',
+        'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/test/12-Alpha/Live/i686/F12-Alpha-i686-Live-KDE.iso',
+        'sha256': '66424c0cbf7974b311070232d49058d0472f90953e71052ef7b2c0d20ebc5c08',
+    },
+
+    {
+        'name': 'Fedora 12 Alpha (x86_64)',
+        'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/test/12-Alpha/Live/x86_64/F12-Alpha-x86_64-Live.iso',
+        'sha256': 'ef2ed63f10039df58fc9ab37b40c4322077cce2cdf3a2bbe1da35c54bd59966f',
+    },
+
+    {
+        'name': 'Fedora 12 Alpha KDE (x86_64)',
+        'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/test/12-Alpha/Live/x86_64/F12-Alpha-x86_64-Live-KDE.iso',
+        'sha256': '1ad2c3f0bf4c60360d9a831ebacb78c8ca75a718db4e3566bae5cb63dbd27d48',
+    },
+
+    {
         'name': 'Fedora 10 (i686)',
         'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/10/Live/i686/F10-i686-Live.iso',
         'sha1': 'c0efba4eb55c142bcda3d78d8a02fada2339a220',
@@ -39,27 +64,6 @@ releases = (
         'name': 'Fedora 10 KDE (x86_64)',
         'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/10/Live/x86_64/F10-x86_64-Live-KDE.iso',
         'sha1': 'a58634cf8dbd9e27db2b9cbf48d18c2e0dfe941e',
-    },
-
-    {
-        'name': 'Fedora 9 (i686)',
-        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/9/Live/i686/Fedora-9-i686-Live.iso',
-        'sha1': '3ea129ebce5fd55499a813aaa8a042735f2aad6d',
-    },
-    {
-        'name': 'Fedora 9 (x86_64)',
-        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/9/Live/x86_64/Fedora-9-x86_64-Live.iso',
-        'sha1': 'eb4aae6befa106e6eaa1a5f1a83192572957178a',
-    },
-    {
-        'name': 'Fedora 9 KDE (i686)',
-        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/9/Live/i686/Fedora-9-i686-Live-KDE.iso',
-        'sha1': '53e7a022f9f6bb8fc160f4785bddf1c10f317adb',
-    },
-    {
-        'name': 'Fedora 9 KDE (x86_64)',
-        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/9/Live/x86_64/Fedora-9-x86_64-Live-KDE.iso',
-        'sha1': 'ebb037e50b16e9e4741c49b98a44e223cf77e236',
     },
 
     ##
