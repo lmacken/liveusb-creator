31345fc52d2607d3da3b2ca038203a30ee7b1723
Luke Macken <lmacken@redhat.com>
2010-11-22 11:58:51 -0500

    Add Sugar on a Stick v4.0 to the release list

--- liveusb/releases.py
+++ liveusb/releases.py
@@ -63,65 +63,29 @@ releases = (
         'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/12/Live/x86_64/Fedora-12-x86_64-Live-KDE.iso',
         'sha256': '623c9e8ca7755d9e7723519992b4fa7d0df22ec00b4490bec8e16f64b1dd8c19',
     },
-    {
-        'name': 'Fedora 11 (i686)',
-        'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/11/Live/i686/Fedora-11-i686-Live.iso',
-        'sha256': '6bbb8842bfd64e948cccd4046394a1ab67a18f62e97c36133a8fe1a10c40cfcb',
-    },
-    {
-        'name': 'Fedora 11 KDE (i686)',
-        'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/11/Live/i686/Fedora-11-i686-Live-KDE.iso',
-        'sha256': '543bb77cc3e2618f6d445ac346b046d0520df35cd6444c5040aebb85ca7fa2eb',
-    },
-    {
-        'name': 'Fedora 11 (x86_64)',
-        'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/11/Live/x86_64/Fedora-11-x86_64-Live.iso',
-        'sha256': 'f1ca7029c35dbbcbbfd16b959bf6c2c5de6c309d60a6e87a113ee4dba4124c26',
-    },
-    {
-        'name': 'Fedora 11 KDE (x86_64)',
-        'url': 'http://download.fedora.redhat.com/pub/fedora/linux/releases/11/Live/x86_64/Fedora-11-x86_64-Live-KDE.iso',
-        'sha256': '4fdf952afb0d27887639140f6921ff58ba74f8d633b14414b095eb54b55df405',
-    },
-
-    {
-        'name': 'Fedora 10 (i686)',
-        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/10/Live/i686/F10-i686-Live.iso',
-        'sha1': 'c0efba4eb55c142bcda3d78d8a02fada2339a220',
-    },
-    {
-        'name': 'Fedora 10 KDE (i686)',
-        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/10/Live/i686/F10-i686-Live-KDE.iso',
-        'sha1': 'd1b1d714d77534872b82c97abcf6f6c877a0d364',
-    },
-    {
-        'name': 'Fedora 10 (x86_64)',
-        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/10/Live/x86_64/F10-x86_64-Live.iso',
-        'sha1': '942ed175b97d5194daf71ba27f34444bf7544c1b',
-    },
-    {
-        'name': 'Fedora 10 KDE (x86_64)',
-        'url': 'http://download.fedoraproject.org/pub/fedora/linux/releases/10/Live/x86_64/F10-x86_64-Live-KDE.iso',
-        'sha1': 'a58634cf8dbd9e27db2b9cbf48d18c2e0dfe941e',
-    },
 
     ##
     ## Custom spins
     ##
-    {
-        'name': 'Sugar on a Stick v1 Strawberry',
-        'url': 'http://download.sugarlabs.org/soas/releases/soas-1-strawberry.iso',
-        'sha1': '41b08c93a65cc1e38286b9f8980f51528a36761d',
-    },
 
+    {
+        'name': 'Sugar on a Stick v4 Mango lassi',
+        'url': 'http://alt.fedoraproject.org/pub/alt/spins/linux/releases/14/Spins/i686/Fedora-14-i686-Live-SoaS.iso',
+        'sha256': '32d74ea3dee51ea4643b7ea0143774099533e727f301e7868d399366f26c5471',
+    },
+    {
+        'name': 'Sugar on a Stick v3 Mirabelle',
+        'url': 'http://alt.fedoraproject.org/pub/alt/spins/linux/releases/13/Spins/i686/Fedora-13-i686-Live-SoaS.iso',
+        'sha1': 'b610e7eb6f8128295b643cdfc32101731d8d3f22',
+    },
     {
         'name': 'Sugar on a Stick v2 Blueberry',
         'url': 'http://download.sugarlabs.org/soas/releases/soas-2-blueberry.iso',
         'sha1': '8022b68433295f12fbfffa66bf49205851a6a1e0',
     },
     {
-        'name': 'Sugar on a Stick v3 Mirabelle',
-        'url': 'http://alt.fedoraproject.org/pub/alt/spins/linux/releases/13/Spins/i686/Fedora-13-i686-Live-SoaS.iso',
-        'sha1': 'b610e7eb6f8128295b643cdfc32101731d8d3f22',
+        'name': 'Sugar on a Stick v1 Strawberry',
+        'url': 'http://download.sugarlabs.org/soas/releases/soas-1-strawberry.iso',
+        'sha1': '41b08c93a65cc1e38286b9f8980f51528a36761d',
     },
 )
