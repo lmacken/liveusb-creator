a3e54a38f19a2228b6266437055b16ad4eec0998
Luke Macken <lmacken@redhat.com>
2011-01-05 19:47:58 -0500

    Add a py2exe win32com.shell hack to our setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,31 @@ if os.path.exists('po/locale'):
         locales.append(os.path.join(lang, 'LC_MESSAGES'))
 
 if sys.platform == 'win32':
+
+    # win32com.shell fix from http://www.py2exe.org/index.cgi/win32com.shell
+    # ModuleFinder can't handle runtime changes to __path__, but win32com uses them
+    try:
+        # py2exe 0.6.4 introduced a replacement modulefinder.
+        # This means we have to add package paths there, not to the built-in
+        # one.  If this new modulefinder gets integrated into Python, then
+        # we might be able to revert this some day.
+        # if this doesn't work, try import modulefinder
+        try:
+            import py2exe.mf as modulefinder
+        except ImportError:
+            import modulefinder
+        import win32com
+        for p in win32com.__path__[1:]:
+            modulefinder.AddPackagePath("win32com", p)
+        for extra in ["win32com.shell"]: #,"win32com.mapi"
+            __import__(extra)
+            m = sys.modules[extra]
+            for p in m.__path__[1:]:
+                modulefinder.AddPackagePath(extra, p)
+    except ImportError:
+        # no build path setup, no worries.
+        pass
+
     import py2exe
     LOCALE_DIR = 'locale'
 
