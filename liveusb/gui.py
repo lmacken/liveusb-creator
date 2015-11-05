ea5a557fd356db7657e610ffc9fc8b38b89855a8
Martin Briza <mbriza@redhat.com>
2015-11-05 16:17:04 +0100

    Use cp by default again
    More user friendly especially when trying to remove Fedora from the flash drive

--- liveusb/gui.py
+++ liveusb/gui.py
@@ -788,7 +788,7 @@ class LiveUSBData(QObject):
     _optionNames = {'dd': _('Use <b>dd</b> to write the image - this will erase everything on your portable drive'),
                     'resetMBR': _('Reset the MBR (Master Boot Record)'),
                    }
-    _optionValues = {'dd': True,
+    _optionValues = {'dd': False,
                      'resetMBR': True,
                     }
 
